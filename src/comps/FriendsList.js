74921e13176cbc0dea517e6336007c521fd4a3be
Deniz <77787415+deniz244@users.noreply.github.com>
2023-08-08 15:00:19 +0300

    axios yapısı denendi

--- src/comps/FriendsList.js
+++ src/comps/FriendsList.js
@@ -1,4 +1,19 @@
+import axios from "axios";
+
 export default function FriendsList() {
+  axios
+    .get("http://localhost:9000/api/friends")
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
   return (
     <>
       <h1 className="text-xl font-bold p-2"> FRIENDS LIST</h1>
--- src/comps/Logout.js
+++ src/comps/Logout.js
@@ -1,3 +1,10 @@
+import axios from "axios";
+
 export default function Logout() {
+  axios({
+    method: "post",
+    url: "http://localhost:9000/api/logout",
+    data: {},
+  });
   return <></>;
 }
